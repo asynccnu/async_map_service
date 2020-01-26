58cfeb0d6abe10dc96fc873cf1d0130509e9ee41
Humbertzhang <504490160@qq.com>
2020-01-26 16:07:14 +0800

    Update mongoDB.py

--- service/mongoDB.py
+++ service/mongoDB.py
@@ -1,14 +1,11 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGODB_HOST = os.getenv('MONGODB_HOST') or 'localhost'
-MONGODB_PORT = int(os.getenv('MONGODB_PORT') or '27017')
-MONGODB_USERNAME = os.getenv('MONGODB_USERNAME') or "muxi"
-MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD') or "nopassword"
+MONGO_URI = os.environ.get("MONGO_URI") or "mongodb://username:secret@localhost:27017/?authSource=admin"
 
 async def db_setup():
     # client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
-    mongo_uri = "mongodb://{}:{}@{}:{}".format(MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT)
+    mongo_uri = MONGO_URI
     client = AsyncIOMotorClient(mongo_uri)
     atlas = client['atlas']
     plat = atlas['plat']
